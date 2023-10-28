662ad42782a93cd5d81586a449b36e83f3a62914
mebsport <56307645+mebsport@users.noreply.github.com>
2023-10-28 16:11:16 -0400

    Update ComingSoon.tsx

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
 import Blink from 'react-blink-text';
 
-function ComingSoon(){
-    return (
-      <div className="Placeholder">
-        <Blink color='blue' text='Coming Soon' fontSize='20'>
-          Testing the Blink
-        </Blink> 
-      </div>
-    );
+function ComingSoon() {
+  return (
+    <div className='Placeholder'>
+      <Blink color='blue' text='Coming Soon' fontSize='20'>
+        Testing the Blink
+      </Blink>
+    </div>
+  );
 }
 export default ComingSoon;
